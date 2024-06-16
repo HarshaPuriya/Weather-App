6d1e61a60b79f9b10dc393aef34504e3c522bcf9
Harsha Puriya <harshapuriya@gmail.com>
2024-06-16 17:21:23 +0530

    ready to deploy

---
+++ script.js
@@ -0,0 +1,42 @@
+// let btn = document.querySelector("button");
+let form = document.querySelector("form");
+let input = document.querySelector("input");
+let temp = document.querySelector('.temp');
+let location_p = document.querySelector('.time_location p')
+let time_date = document.querySelector('.time_location span')
+let condition = document.querySelector('.weather_condition span')
+let weather_image = document.querySelector('.weather_condition img')
+
+// btn.addEventListener('click', function() {
+//     let value = input.value;
+//     console.log(value);
+// })
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  let value = input.value;
+  console.log(value);
+  let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=e36480d0944e4c3a9b1164335233012&q='+value+'&aqi=no';
+  fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    temp.innerText = data.current.temp_c;
+    location_p.innerText = data.location.name;
+    time_date.innerText = data.location.localtime;
+    condition.innerText = data.current.condition.text;
+    let imgSrc = data.current.condition.icon;
+    weather_image.src = "https:"+imgSrc;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("Sorry for the inconvience, please try again with correct name");
+  });
+  
+})
+ 
